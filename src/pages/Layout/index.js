bce0c29571dfe15bb0b6c1967b4bda61fc090304
archiecheng <archiechengice@outlook.com>
2024-02-04 21:56:20 -0800

    完成主题色的定制化

---
+++ src/pages/Layout/index.js
@@ -0,0 +1,18 @@
+import { Button } from "antd-mobile"
+import { Outlet } from "react-router-dom"
+
+const Layout = () => {
+    return (
+        <div>
+            <Outlet />
+            Layout
+            {/* 全局生效样式 */}
+            <Button color="primary">测试全局</Button>
+            <div className="purple">
+                <Button color="primary">测试局部</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Layout
