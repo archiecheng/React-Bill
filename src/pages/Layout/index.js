7be185a77d727c4ee7b744b4f28714bcb9b68f96
archiecheng <archiechengice@outlook.com>
2024-02-05 08:39:02 -0800

    tabBar切换功能

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,24 +1,59 @@
-import { Button } from "antd-mobile"
-import { useEffect } from "react"
-import { Outlet } from "react-router-dom"
-import { useDispatch } from "react-redux"
-import { getBillList } from "../../store/modules/billStore"
-const Layout = () => {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getBillList())
-    }, [dispatch])
-    return (
-        <div>
-            <Outlet />
-            Layout
-            {/* 全局生效样式 */}
-            <Button color="primary">测试全局</Button>
-            <div className="purple">
-                <Button color="primary">测试局部</Button>
-            </div>
-        </div>
-    )
-}
+import { Button, TabBar } from "antd-mobile";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getBillList } from "../../store/modules/billStore";
 
-export default Layout
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+import "./index.scss";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
+const Layout = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBillList());
+  }, [dispatch]);
+  // 切换菜单跳转路由
+  const navigate = useNavigate()
+  const switchRouter = (path) => {
+    navigate(path)
+  }
+  return (
+    <div>
+      <div className="layout">
+        <div className="container">
+          <Outlet />
+        </div>
+        <div className="footer">
+          <TabBar onChange={switchRouter}>
+            {tabs.map((item) => (
+              <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+            ))}
+          </TabBar>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
