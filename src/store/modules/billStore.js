7789478c91424e802b1f6e25b52b3f29a79716ce
archiecheng <archiechengice@outlook.com>
2024-02-05 12:10:28 -0800

    新增功能实现

--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -11,12 +11,16 @@ const billStore = createSlice({
         // 同步修改的方法
         setBillList(state, action) {
             state.billList = action.payload
+        },
+        // 同步添加账单方法
+        addBill(state, action) {
+            state.billList.push(action.payload)
         }
     }
 })
 
 // 解构actionCreater函数
-const { setBillList } = billStore.actions
+const { setBillList, addBill } = billStore.actions
 // 编写异步
 const getBillList = () => {
     return async (dispatch) => {
@@ -27,7 +31,16 @@ const getBillList = () => {
     }
 }
 
-export { getBillList }
+const addBillList = (data) => {
+    return async (dispatch) => {
+        // 编写异步请求
+        // 触发同步reducer
+        const res = await axios.post('http://localhost:8888/ka', data)
+        dispatch(addBill(res.data))
+    }
+}
+
+export { getBillList, addBillList }
 
 // 导出reducer
 const reducer = billStore.reducer
